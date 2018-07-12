27e2b2d88820333bad608a0a8365fcfeadb1cb88
manikandanrohit <41136817+manikandanrohit@users.noreply.github.com>
2018-07-12 12:00:36 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,14 @@
+package mani;
+import java.util.Scanner;
+public class positive {
+public static void main(String[] args) {
+	Scanner in=new Scanner (System.in);
+	int a=in.nextInt();
+	if(a%2==0){
+		System.out.println("even");
+	}else{
+		System.out.println("odd");
+	}
+			
+}
+}
