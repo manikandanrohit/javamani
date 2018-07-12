02d9d66690f242e71051af612c64ff3134be3c7e
manikandanrohit <41136817+manikandanrohit@users.noreply.github.com>
2018-07-12 16:19:27 +0530

    Update and rename vowels.java to alphapets.java

--- alphapets.java
+++ alphapets.java
@@ -2,14 +2,14 @@ package mani;
 
 import java.util.Scanner;
 
-public class consonant {
+public class alphapets {
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
 	int a=in.next().charAt(0);
 	if(a>=65&&a<=90 ||a>=97&&a<=122){
-	System.out.println("consonant");
+	System.out.println("yes");
 }else{
-	System.out.println("invalid");
+	System.out.println("no");
 }
 }
 }
